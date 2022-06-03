e5da0eca41e9aec035a658bbea4502387830b747
jjrx <jjohal52@gmail.com>
2022-06-03 16:43:04 -0700

    implement PUT /loads/load_id

--- loads.js
+++ loads.js
@@ -17,26 +17,26 @@ router.use(bodyParser.json());
 // add a new load entity
 function post_load(vendor, item, quantity, weight) {
   var key = datastore.key(LOAD);
-  const new_truck = {
+  const new_load = {
     vendor,
     item,
     quantity,
     weight,
     carrier: null,
   };
-  return datastore.save({ key: key, data: new_truck }).then(() => {
+  return datastore.save({ key: key, data: new_load }).then(() => {
     return key;
   });
 }
 
-function put_load(id, name, volume, item, creation_date) {
+function put_load(id, vendor, item, quantity, weight) {
   const key = datastore.key([LOAD, parseInt(id, 10)]);
   const load = {
-    name: name,
-    volume: volume,
+    vendor,
+    item,
+    quantity,
+    weight,
     carrier: null,
-    item: item,
-    creation_date: creation_date,
   };
   return datastore.save({ key: key, data: load });
 }
@@ -135,13 +135,61 @@ router.post("/", function (req, res) {
 });
 
 router.put("/:id", function (req, res) {
-  put_load(
-    req.params.id,
-    req.body.name,
-    req.body.volume,
-    req.body.item,
-    req.body.creation_date
-  ).then(res.status(200).end());
+  // reject requests that aren't JSON
+  if (req.get("content-type") !== "application/json") {
+    return res
+      .status(415)
+      .json({ Error: "Server only accepts application/json data." });
+  }
+
+  const accepts = req.accepts(["application/json"]);
+  if (!accepts) {
+    return res.status(406).json({
+      Error: "This application only supports JSON responses",
+    });
+  }
+
+  const load_id = req.params.id;
+
+  ds.getEntityByID(LOAD, load_id).then((load) => {
+    if (load[0] === undefined || load[0] === null) {
+      res.status(404).json({
+        Error: "No load with this load_id exists",
+      });
+    } else {
+      // ignore any extraneous attributes by only extracting relevant values from request
+      const load_values = [
+        req.body.vendor,
+        req.body.item,
+        req.body.quantity,
+        req.body.weight,
+      ];
+
+      // ensure all required attributes are included in the request
+      if (!ds.hasFalsyValue(load_values)) {
+        // remove load from truck list of loads if applicable
+        const truck_id = load[0].carrier;
+        if (truck_id) {
+          ds.removeLoadFromTruck(truck_id, load_id);
+        }
+        put_load(load_id, ...load_values).then(() => {
+          // get the load that was just created
+          ds.getEntityByID(LOAD, load_id).then((load) => {
+            res.status(200).send({
+              ...load[0],
+              // modify reponse to include self link for load
+              self: `${req.protocol}://${req.get("host")}/loads/${load_id}`,
+            });
+          });
+        });
+      } else {
+        res.status(400).json({
+          Error:
+            "The request object is missing at least one of the required attributes",
+        });
+      }
+    }
+  });
 });
 
 router.delete("/:id", function (req, res) {
